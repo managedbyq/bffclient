fcfba4445159bce1bd6d53006aed1458929ff59d
Erty Seidohl <eseidohl@managedbyq.com>
2018-01-04 12:42:02 -0500

    Part 1: FlatFileTokenStore detect key changes (#11)
    * Part 1: FlatFileTokenStore detect key changes
    
    * lint
    
    * Fix flaky tests

--- tests/flatfiletokenstore.spec.js
+++ tests/flatfiletokenstore.spec.js
@@ -21,4 +21,26 @@ describe('FlatFileTokenStore', () => {
 
     mockFs.restore();
   });
+
+  it('should detect when the file store changes', async () => {
+    mockFs({
+      PATH: '',
+    });
+
+    const ffts = new FlatFileTokenStore(PATH);
+    await ffts.storeToken('key', 'TOKEN_VALUE');
+
+    const ffts2 = new FlatFileTokenStore(PATH);
+    await ffts2.storeToken('key', 'NEW_TOKEN_VALUE');
+
+    const newToken = await ffts.getToken('key');
+    assert.strictEqual(newToken, 'NEW_TOKEN_VALUE');
+
+    await ffts.storeToken('key', 'NEWER_TOKEN_VALUE');
+
+    const newerToken = await ffts2.getToken('key');
+    assert.strictEqual(newerToken, 'NEWER_TOKEN_VALUE');
+
+    mockFs.restore();
+  });
 });
