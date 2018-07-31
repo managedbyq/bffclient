782b96b88d791b54849f26fefbbab07f94129617
dursk <dursk@users.noreply.github.com>
2018-07-31 11:40:29 -0400

    New redis lib (#19)
    * Change redis lib
    
    * version bump
    
    * fix packagejson
    
    * linttt

--- tests/redistokenstore.spec.js
+++ tests/redistokenstore.spec.js
@@ -1,11 +1,11 @@
 const assert = require('chai').assert;
-const fakeRedis = require('fakeredis');
+const FakeRedis = require('ioredis-mock');
 
 const RedisTokenStore = require('../stores/redistokenstore');
 
 describe('RedisTokenStore', () => {
   it('Should store tokens', async () => {
-    const rts = new RedisTokenStore('fake_redis', fakeRedis);
+    const rts = new RedisTokenStore('fake_redis', new FakeRedis());
     await rts.storeToken('key', 'TOKEN_VALUE');
     const token = await rts.getToken('key');
     assert.strictEqual(token, 'TOKEN_VALUE');
