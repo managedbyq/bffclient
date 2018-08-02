ddc2f548c020c24164e4e106cca72a4fd8815176
dursk <dursk@users.noreply.github.com>
2018-08-02 16:43:15 -0400

    Refactor for flexibility (separate client and tokenmanager) (#20)
    * Refactor for flexibility
    
    * version bump

--- tests/serviceclient.spec.js
+++ tests/serviceclient.spec.js
@@ -1,98 +1,20 @@
 const assert = require('chai').assert;
 const nock = require('nock');
 
-const utils = require('../utils');
 const MockTokenStore = require('./mocktokenstore');
-
-describe('ServiceClientFactory', () => {
-  let ServiceClientFactory;
-
-  beforeEach(() => {
-    // eslint-disable-next-line global-require
-    ServiceClientFactory = require('../index');
-    nock.disableNetConnect();
-  });
-
-  afterEach(() => {
-    nock.cleanAll();
-    nock.enableNetConnect();
-  });
-
-  it('should not create a ServiceClient when uninitialized', () => {
-    assert.throws(() => ServiceClientFactory.createServiceClient(
-      'uninitialized_service_client_factory',
-      'http://example.com',
-      'audience_string',
-    ), 'Cannot create client in uninitialized ServiceClientFactory');
-  });
-
-  it('should create a ServiceClient when initialized', async () => {
-    ServiceClientFactory.init({
-      auth0Domain: 'auth0.example.com',
-      auth0Client: 'TEST_CLIENT_ID_INIT',
-      auth0Secret: 'TEST_CLIENT_SECRET_INIT',
-      tokenStore: new MockTokenStore(),
-    });
-    const sc = ServiceClientFactory.createServiceClient(
-      'initialized_service_client_factory',
-      'http://example.com',
-      'audience_string',
-    );
-    assert.isOk(sc);
-    assert.isTrue(nock.isDone());
-  });
-
-  it('should create a ServiceClient with a correlation ID getter', async () => {
-    ServiceClientFactory.init({
-      auth0Domain: 'auth0.example.com',
-      auth0Client: 'TEST_CLIENT_ID_INIT',
-      auth0Secret: 'TEST_CLIENT_SECRET_INIT',
-      tokenStore: new MockTokenStore(),
-      getCorrelationId: () => 'correlation_id',
-    });
-    const sc = ServiceClientFactory.createServiceClient(
-      'service_with_correlation_id_getter',
-      'http://example.com',
-      'audience_string',
-    );
-    assert.isOk(sc);
-    assert.isFunction(sc.getCorrelationId);
-    assert.notStrictEqual(sc.getCorrelationId, utils.noop);
-    assert.isTrue(nock.isDone());
-  });
-});
+const ServiceClient = require('../client');
 
 describe('ServiceClient without correlation IDs', () => {
-  let ServiceClientFactory;
   let serviceClient;
+  let mockedTokenManager;
 
   beforeEach(async () => {
-    nock('https://auth0.example.com')
-      .post(
-        '/oauth/token',
-        {
-          grant_type: /.*/,
-          client_id: 'TEST_CLIENT_ID',
-          client_secret: 'TEST_CLIENT_SECRET',
-          audience: 'audience_string',
-        },
-      )
-      .reply(200, {
-        access_token: 'BEARER_TOKEN',
-      });
-
-    // eslint-disable-next-line global-require
-    ServiceClientFactory = require('../index');
-    ServiceClientFactory.init({
-      auth0Domain: 'auth0.example.com',
-      auth0Client: 'TEST_CLIENT_ID',
-      auth0Secret: 'TEST_CLIENT_SECRET',
-      tokenStore: new MockTokenStore(),
-    });
-    serviceClient = ServiceClientFactory.createServiceClient(
-      'service_client',
+    mockedTokenManager = new MockTokenStore();
+    mockedTokenManager.storeToken('tokenName', 'BEARER_TOKEN');
+    serviceClient = new ServiceClient(
       'https://example.com',
-      'audience_string',
+      mockedTokenManager,
+      'tokenName',
     );
   });
 
@@ -204,39 +126,6 @@ describe('ServiceClient without correlation IDs', () => {
     assert.isTrue(nock.isDone());
   });
 
-  it('should be able to refresh its access tokens', async () => {
-    nock('https://example.com')
-      .matchHeader('authorization', 'Bearer BEARER_TOKEN')
-      .get('/token_test')
-      .reply(200, { ok: true });
-
-    await serviceClient.get('/token_test');
-
-    nock('https://auth0.example.com')
-      .post(
-        '/oauth/token',
-        {
-          grant_type: /.*/,
-          client_id: 'TEST_CLIENT_ID',
-          client_secret: 'TEST_CLIENT_SECRET',
-          audience: 'audience_string',
-        },
-      )
-      .reply(200, {
-        access_token: 'ANOTHER_TOKEN',
-      });
-
-    await serviceClient.refreshToken();
-
-    nock('https://example.com')
-      .matchHeader('authorization', 'Bearer ANOTHER_TOKEN')
-      .get('/token_test_2')
-      .reply(200, { ok: true });
-
-    await serviceClient.get('/token_test_2');
-    assert.isTrue(nock.isDone());
-  });
-
   it('should repeat multiple query params by default', async () => {
     nock('https://example.com')
       .matchHeader('authorization', 'Bearer BEARER_TOKEN')
@@ -268,29 +157,16 @@ describe('ServiceClient without correlation IDs', () => {
 
 describe('ServiceClient with correlation IDs', () => {
   let correlatingClient;
+  let mockedTokenManager;
 
   beforeEach(async () => {
-    nock('https://auth0.example.com')
-      .post('/oauth/token', {
-        grant_type: /.*/,
-        client_id: 'TEST_CLIENT_ID',
-        client_secret: 'TEST_CLIENT_SECRET',
-        audience: 'audience_string',
-      }).reply(200, { access_token: 'BEARER_TOKEN' });
-
-    // eslint-disable-next-line global-require
-    const ServiceClientFactory = require('../index');
-    ServiceClientFactory.init({
-      auth0Domain: 'auth0.example.com',
-      auth0Client: 'TEST_CLIENT_ID',
-      auth0Secret: 'TEST_CLIENT_SECRET',
-      tokenStore: new MockTokenStore(),
-      getCorrelationId: () => 'abc123',
-    });
-    correlatingClient = ServiceClientFactory.createServiceClient(
-      'service_client',
+    mockedTokenManager = new MockTokenStore();
+    mockedTokenManager.storeToken('tokenName', 'BEARER_TOKEN');
+    correlatingClient = new ServiceClient(
       'https://example.com',
-      'audience_string',
+      mockedTokenManager,
+      'tokenName',
+      () => 'abc123',
     );
   });
 
