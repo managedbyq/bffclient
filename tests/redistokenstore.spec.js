3cbcd061bfb0692866963ef54cfadfd188e24013
Taylor Burgess <tayloraburgess@users.noreply.github.com>
2018-11-21 10:24:51 -0500

    Move back to node-redis (#21)
    * Move back to node-redis
    
    * Lint

--- tests/redistokenstore.spec.js
+++ tests/redistokenstore.spec.js
@@ -1,11 +1,11 @@
 const assert = require('chai').assert;
-const FakeRedis = require('ioredis-mock');
+const client = require('fakeredis').createClient('fake_redis');
 
 const RedisTokenStore = require('../stores/redistokenstore');
 
 describe('RedisTokenStore', () => {
   it('Should store tokens', async () => {
-    const rts = new RedisTokenStore('fake_redis', new FakeRedis());
+    const rts = new RedisTokenStore('fake_redis', client);
     await rts.storeToken('key', 'TOKEN_VALUE');
     const token = await rts.getToken('key');
     assert.strictEqual(token, 'TOKEN_VALUE');
